27ba8729087497d634a596607780195b489b9780
12Wass <wassimdah@gmail.com>
2023-02-17 23:28:37 -0800

    Initial commit : Project generated and first components created

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import React, { PropsWithChildren } from "react";
+
+export const Button = ({children}: PropsWithChildren<any>) => {
+    return (
+        <button>Ceci est un bouton</button>
+    )
+}
