93c9dbca31cc92d19f7c5e66875f4aec04368eb7
12Wass <wassimdah@gmail.com>
2023-02-26 23:47:13 -0800

    refactoring register form - improving state management, adding memorization of values on form and dynamic state modifications

--- src/components/Register/Forms/InformationsForm.tsx
+++ src/components/Register/Forms/InformationsForm.tsx
@@ -11,11 +11,11 @@ import {
   InputRightElement,
 } from "@chakra-ui/react";
 import { UserInfosProps } from "../../../helpers/interfaces/UserInfos.props";
-
+import { UserInfosEnum } from "../../../helpers/enums/UserInfos.enum";
 export const InformationsForm = (props: UserInfosProps) => {
   const [show, setShow] = React.useState(false);
   const handlePassword = () => setShow(!show);
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  console.log(props.userInfos);
   return (
     <>
       <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
@@ -26,28 +26,68 @@ export const InformationsForm = (props: UserInfosProps) => {
           <FormLabel htmlFor="pseudo" fontWeight={"normal"}>
             Pseudo
           </FormLabel>
-          <Input id="pseudo" placeholder="Pseudo" onChange={handleChange} />
+          <Input
+            id="pseudo"
+            placeholder="Pseudo"
+            value={props?.userInfos?.pseudo}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              props.setUserInfos({
+                ...props.userInfos,
+                pseudo: e.target.value,
+              });
+            }}
+          />
         </FormControl>
 
         <FormControl mr="5%">
           <FormLabel htmlFor="firstName" fontWeight={"normal"}>
             Prénom
           </FormLabel>
-          <Input id="firstName" placeholder="Prénom" onChange={handleChange} />
+          <Input
+            id="firstName"
+            placeholder="Prénom"
+            value={props?.userInfos?.firstName}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              props.setUserInfos({
+                ...props.userInfos,
+                firstName: e.target.value,
+              })
+            }
+          />
         </FormControl>
 
         <FormControl>
           <FormLabel htmlFor="lastName" fontWeight={"normal"}>
             Nom
           </FormLabel>
-          <Input id="lastName" placeholder="Nom" onChange={handleChange} />
+          <Input
+            id="lastName"
+            placeholder="Nom"
+            value={props?.userInfos?.lastName}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              props.setUserInfos({
+                ...props.userInfos,
+                lastName: e.target.value,
+              })
+            }
+          />
         </FormControl>
       </Flex>
       <FormControl mt="2%">
         <FormLabel htmlFor="email" fontWeight={"normal"}>
           Adresse e-mail
         </FormLabel>
-        <Input id="email" type="email" onChange={handleChange} />
+        <Input
+          id="email"
+          type="email"
+          value={props?.userInfos?.email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            props.setUserInfos({
+              ...props.userInfos,
+              email: e.target.value,
+            })
+          }
+        />
         <FormHelperText>
           Nous ne partagerons jamais votre adresse.
         </FormHelperText>
@@ -62,7 +102,13 @@ export const InformationsForm = (props: UserInfosProps) => {
             pr="4.5rem"
             type={show ? "text" : "password"}
             placeholder="Mot de passe"
-            onChange={handleChange}
+            value={props?.userInfos?.password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              props.setUserInfos({
+                ...props.userInfos,
+                password: e.target.value,
+              })
+            }
           />
           <InputRightElement width="4.5rem">
             <Button h="1.75rem" size="sm" onClick={handlePassword}>
@@ -81,7 +127,13 @@ export const InformationsForm = (props: UserInfosProps) => {
             id="confirmation"
             type="password"
             placeholder="Mot de passe"
-            onChange={handleChange}
+            value={props?.userInfos?.confirmation}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              props.setUserInfos({
+                ...props.userInfos,
+                confirmation: e.target.value,
+              })
+            }
           />
         </InputGroup>
       </FormControl>
